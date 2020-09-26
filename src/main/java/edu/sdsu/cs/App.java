e1eb0010af7e22ec290f884879e4b097f651dd6e
zKavtaradze <ZurabKavtaradze@protonmail.com>
2020-09-26 18:32:49 +0400

    initial setup

---
+++ src/main/java/edu/sdsu/cs/App.java
@@ -0,0 +1,13 @@
+package edu.sdsu.cs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
