e8ae35851e160f389ccdb2c762ad31ad0cb7f6e7
zKavtaradze <ZurabKavtaradze@protonmail.com>
2020-09-28 20:53:00 +0400

    refactor

--- src/main/java/edu/sdsu/cs/ServiceProvider.java
+++ src/main/java/edu/sdsu/cs/ServiceProvider.java
@@ -30,18 +30,26 @@ class ServiceProvider
         else
             return;
 
-        boolean s = new File(path).exists();
         FileWriter writer = new FileWriter(path, false);
 
-        String data = String.format("%20s:%d" , "Length of longest line in file" , statistics.getLengthOfLongestLine())
-                    .concat(String.format("%20s:%.2f" , "\nAverage line length" , statistics.getAverageLineLength()))
-                    .concat(String.format("%20s:%d" , "\nNumber of unique space-delineated tokens (case-sensitive)" , statistics.getSensitiveNumberOfSpaceDelimitedTokens()))
-                    .concat(String.format("%20s:%d" , "\nNumber of unique space-delineated tokens (case-insensitive)" , statistics.getInsensitiveNumberOfSpaceDelimitedTokens()))
-                    .concat(String.format("%20s:%d" , "\nNumber of all space-delineated tokens in file" , statistics.getNumberOfAllSpaceDelimitedTokens()))
-                    .concat(String.format("%20s:%s" , "\nMost frequently occurring token(s)" , statistics.getMostFrequentlyOccuringToken()))
-                    .concat(String.format("%20s:%d" , "\nCount of most frequently occurring token (case-insensitive)" , statistics.getInsensitiveCountOfMostFrequentlyOccuringToken()))
-                    .concat(String.format("%20s:%s" , "\n10 most frequent tokens with their counts (case-insensitive)" , GetFormatedString(statistics.getInsensitiveTenMostFrequentlyUsedTokens())))
-                    .concat(String.format("%20s:%s" , "\n10 least frequent tokens with their counts (case-sensitive)" , GetFormatedString(statistics.getInsensitiveTenLeastFrequentlyUsedTokens())));
+        String data = String.format("%20s:%d" , "Length of longest line in file",
+                statistics.getLengthOfLongestLine())
+                    .concat(String.format("%20s:%.2f" , "\nAverage line length",
+                            statistics.getAverageLineLength()))
+                    .concat(String.format("%20s:%d" , "\nNumber of unique space-delineated tokens (case-sensitive)",
+                            statistics.getSensitiveNumberOfSpaceDelimitedTokens()))
+                    .concat(String.format("%20s:%d" , "\nNumber of unique space-delineated tokens (case-insensitive)",
+                            statistics.getInsensitiveNumberOfSpaceDelimitedTokens()))
+                    .concat(String.format("%20s:%d" , "\nNumber of all space-delineated tokens in file",
+                            statistics.getNumberOfAllSpaceDelimitedTokens()))
+                    .concat(String.format("%20s:%s" , "\nMost frequently occurring token(s)",
+                            statistics.getMostFrequentlyOccuringToken()))
+                    .concat(String.format("%20s:%d" , "\nCount of most frequently occurring token (case-insensitive)",
+                            statistics.getInsensitiveCountOfMostFrequentlyOccuringToken()))
+                    .concat(String.format("%20s:%s" , "\n10 most frequent tokens with their counts (case-insensitive)",
+                            GetFormatedString(statistics.getInsensitiveTenMostFrequentlyUsedTokens())))
+                    .concat(String.format("%20s:%s" , "\n10 least frequent tokens with their counts (case-sensitive)",
+                            GetFormatedString(statistics.getInsensitiveTenLeastFrequentlyUsedTokens())));
 
         writer.write(data);
 
